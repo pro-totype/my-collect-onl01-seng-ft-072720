2a55e790f7a6e3559e158932a1f80b6d801ddc78
Sean B <butts.sean01@gmail.com>
2020-07-31 23:28:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,8 @@ def my_collect(array)
   
    empty_array = []
     counter = 0
-      my_collect(empty_array) yield 
+      my_collect(empty_array) yield (|x| x*0)
         counter += 1
+        yield
       end
-    expect(counter).to eq(0)
+      end
